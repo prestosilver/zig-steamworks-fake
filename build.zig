e35597e768863b9d19698e8948bd4a336f4f4601
bob16795 <prestosilver.ptp@gmail.com>
2025-12-21 11:05:36 -0500

    Update build.zig

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const mod = b.addModule("zig_steamworks_fake", .{
+    const mod = b.addModule("steam", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
