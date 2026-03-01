db6df569201d30d5067e2be8da40b5a4ef8ada7e
bob16795 <prestosilver.ptp@gmail.com>
2026-03-01 11:02:06 -0500

    These structs should be packed

--- src/callbacks.zig
+++ src/callbacks.zig
@@ -1,6 +1,6 @@
 const steam = @import("root.zig");
 
-pub const CreateItem = extern struct {
+pub const CreateItem = packed struct {
     pub const ID: steam.CallbackId = .create_item;
 
     result: steam.Result,
@@ -10,7 +10,7 @@ pub const CreateItem = extern struct {
     //    04190927 :1441796 > IClientUtils::GetAPICallResult( 0xF0B1C18BA0C46DA5, 24, 3403, ) = 1, 24 bytes [01 00 00 00 4d 85 3f d0 00 00 00 00 00 74 65 6d 00 00 00 00 00 00 00 00], 0,
 };
 
-pub const UpdateItem = extern struct {
+pub const UpdateItem = packed struct {
     pub const ID: steam.CallbackId = .update_item;
 
     result: steam.Result,
